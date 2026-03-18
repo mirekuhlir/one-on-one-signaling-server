82007c71e1b9e0d9932ea8baba9e786597dcfbde
Miroslav Uhlir <mirek.uhlir@gmail.com>
2026-03-18 19:25:17 +0100

    Add example environment file and update .gitignore to exclude it

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const { PeerServer } = require("peer");
 const port = process.env.PORT || 9000;
 const allowedOrigins = process.env.ALLOWED_ORIGIN ? process.env.ALLOWED_ORIGIN.split(',').map(origin => origin.trim()) : [];
 
-PeerServer({ 
-    port: port, 
+PeerServer({
+    port: port,
     path: "/",
     proxied: true,
+    key: process.env.PEERJS_KEY || "peerjs",
+    allow_discovery: false,
+    concurrent_limit: 1000,
     corsOptions: {
-          origin: allowedOrigins.length === 1 ? allowedOrigins[0] : allowedOrigins
+        origin: allowedOrigins.length >= 1 ? allowedOrigins : false
     }
 });
 
